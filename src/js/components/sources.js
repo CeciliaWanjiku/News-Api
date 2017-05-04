1c807032b5518dd6e62f883eb5a7ba8af981dc54
CeciliaWanjiku <cecilia.wahome@andela.com>
2017-05-04 19:12:22 +0300

    consuming the sources api and mounting it

--- src/js/components/sources.js
+++ src/js/components/sources.js
@@ -1,1 +1,29 @@
-import React from React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+export default class Sources extends React.Component {
+    constructor() {
+        super();
+        this.state = { sources: [] };
+    }
+
+    componentDidMount() {
+        axios.get(`https://newsapi.org/v1/sources?language=en`) 
+        .then((result) => {
+            this.setState({
+                sources:result.data.sources
+            });
+        })   
+    }
+
+    render() {        
+        return(
+            <div>
+                {this.state.sources.map((sources)=> {
+                    return <div>{sources.name} <br /> {sources.description}</div>
+                })}
+           </div>
+        );
+    }
+};
