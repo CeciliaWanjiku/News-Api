a38c91ff6904bb75076be293bf54723fcbcc1b50
CeciliaWanjiku <cecilia.wahome@andela.com>
2017-05-04 17:19:08 +0300

    implementation of shallow tests

---
+++ src/js/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class App extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                {/*<h1> This is Flux</h1>*/}
+                <img src="../img/logo.png"/>
+
+            </div>
+        )
+    }
+}
+export default App;
